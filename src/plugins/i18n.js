33c4d4ce32a2f8da5b39d4176af48052ed8ff4cb
no8ody <no8ody@tanglebay.com>
2020-01-18 00:17:00 +0100

    change names

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -8,22 +8,22 @@ export const i18n = new VueI18n({
   fallbackLocale: 'de',
   messages: {
     en:{
-      register: 'Register your IOTA node and be part of the Community Dock. We support IRI and Hornet nodes.',
+      register: 'Register your IOTA node and be part of the Community. We support IRI and Hornet nodes.',
       register2: 'Register your node',
       nav_register: 'Register',
       join: 'Join the Dock!',
-      nodedock: 'The node dock for the community',
+      nodedock: 'The dock for your node',
       points: 'Points',
       rank: 'Rank',
       imprint: 'Legal Notice',
       privacy: 'Privacy Policy'
     },
     de: {
-      register: 'Registrieren Sie Ihren IOTA-Node und werden Sie Teil des Community Docks. Wir unterstützen IRI- und Hornet-Nodes.',
+      register: 'Registrieren Sie Ihren IOTA-Node und werden Sie Teil der Community. Wir unterstützen IRI- und Hornet-Nodes.',
       register2: 'Registriere deinen Node',
       nav_register: 'Registrieren',
       join: 'Join the Dock!',
-      nodedock: 'Der Nodehafen für die Community',
+      nodedock: 'Der Hafen für deine Node',
       points: 'Punkte',
       rank: 'Rang',
       imprint: 'Impressum',
