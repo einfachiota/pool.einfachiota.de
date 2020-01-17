a13b8d86130d20640002e5c72426d19f7ff6cce3
no8ody <no8ody@tanglebay.com>
2020-01-17 20:04:11 +0100

    change to key

--- src/components/NodeList.vue
+++ src/components/NodeList.vue
@@ -11,9 +11,7 @@
           <template slot-scope="scope">{{ scope.$index + 1 }}</template>
     </el-table-column>
     <el-table-column prop="name" label="Name" align="center"></el-table-column>
-    <el-table-column label="ID" align="center">
-      <template slot-scope="scope">{{ scope.row.nodeKey }}</template>
-    </el-table-column>
+    <el-table-column prop="key" label="ID" align="center"></el-table-column>
     <el-table-column prop="points" label="Points" align="center"></el-table-column>
   </el-table>
 </template>
