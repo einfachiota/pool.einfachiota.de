80d671583689c9d2a022d4a6cc77bf644bd59587
no8ody <no8ody@tanglebay.com>
2020-01-18 11:40:14 +0100

    add milestones to details

--- src/components/NodeDetails.vue
+++ src/components/NodeDetails.vue
@@ -10,6 +10,14 @@
             {{details.key}}
           </p>
           <p>
+            <strong>Latest Milestone:</strong>
+            {{details.milestone}}
+          </p>
+          <p>
+            <strong>Solid Milestone:</strong>
+            {{details.solidMilestone}}
+          </p>
+          <p>
             <strong>Version:</strong>
             {{details.appName}} {{details.appVersion}}
           </p>
