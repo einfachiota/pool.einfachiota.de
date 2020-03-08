0332d655556f29dd76b83e5ac4fbd9ab68085f41
huhn511 <sehe89@gmail.com>
2020-03-08 21:00:40 +0100

    add button styling

--- src/views/Donate.vue
+++ src/views/Donate.vue
@@ -5,19 +5,40 @@
         <h1 class="heading">Donations (BETA)</h1>
         <div class="divider"></div>
         <h2>Einer für alle, alle für einen!</h2>
-        <iota-payment>Donate IOTA</iota-payment>
+        <br />
+        <iota-payment class="iota-pay">Donate IOTA</iota-payment>
       </div>
     </div>
   </div>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
 a {
   text-decoration: none;
   color: var(--primary);
 }
 
 .donate {
-  margin-bottom: 50px;
+  margin: 50px;
+}
+
+.iota-pay {
+  margin-top: 20px;
+  button {
+    font-family: "Open Sans" !important;
+    font-weight: bold !important;
+    text-transform: uppercase !important;
+    letter-spacing: 0.1em !important;
+    border-radius: 10px !important ;
+    padding: 15px 30px !important;
+    transition: all 200ms ease-in-out !important;
+    border: none !important;
+    background-color: var(--primary) !important;
+    color: var(--white) !important;
+    &:hover {
+      background-color: var(--primary-darken) !important;
+      cursor: pointer;
+    }
+  }
 }
 </style>
