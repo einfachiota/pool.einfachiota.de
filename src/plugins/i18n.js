68a31ad58b973993fcfcb94fe6290e886574a4d9
no8ody <no8ody@tanglebay.com>
2020-03-24 00:16:28 +0100

    update register text

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -30,7 +30,7 @@ export const i18n = new VueI18n({
       },
       node_update: {
         title: 'Update node',
-        description: 'To update a node, please fill out the form and run it on your node. If you do not use the same PASSWORD when adding a new node, the node will lose its points in the Dock.',
+        description: 'To update a node, please fill out the form and run it on your node. If you do not use the same PASSWORD when adding a new node, the node will lose its points in the pool.',
       },
       footer: require('./translations/footer').en(),
       donate: {
@@ -52,7 +52,7 @@ export const i18n = new VueI18n({
       privacy: 'Datenschutz',
       node_register: {
         title: 'Node hinzufügen',
-        description: 'Um deine Node der Community über das Tangle Dock zur Verfügung zu stellen, ist es notwendig, ihn dem Load Balancer hinzuzufügen. Es ist nicht notwendig, eine spezielle Software zu installieren oder andere Anwendungen auszuführen.',
+        description: 'Um deine Node der Community über den Pool zur Verfügung zu stellen, ist es notwendig, ihn den Load Balancern hinzuzufügen. Es ist nicht notwendig, eine spezielle Software zu installieren oder andere Anwendungen auszuführen.',
         howto1: 'Um eine Node hinzuzufügen, fülle bitte das Formular aus und führe ihn auf deiner Node aus. Eine CURL-Request wird an die Load Balancer von Tangle Bay gesendet und es wird überprüft, ob die Anforderung tatsächlich von der gewünschten Node stammt und du nicht versuchst, eine fremde Node hinzuzufügen.',
         howto2: 'Bitte führe den Befehl in deiner Konsole aus: ',
         attention: 'Bitte beachte, dass nach dem erfolgreichen Hinzufügen einer neuen Node das erhaltene Passwort sorgfältig gespeichert werden muss. Mit dem Passwort kannst du jederzeit deine Node wieder entfernen.' 
@@ -63,7 +63,7 @@ export const i18n = new VueI18n({
       },
       node_update: {
         title: 'Node updaten',
-        description: 'Um eine Node zu updaten, fülle bitte das Formular aus und führe anschließend den Befehl auf deiner Node aus. Sollte beim wieder hinzufügen nicht das gleiche PASSWORD verwendet werden, verliert die Node ihre Punkte im Dock.'
+        description: 'Um eine Node zu updaten, fülle bitte das Formular aus und führe anschließend den Befehl auf deiner Node aus. Sollte beim wieder hinzufügen nicht das gleiche PASSWORD verwendet werden, verliert die Node ihre Punkte im Pool.'
       },
       footer: require('./translations/footer').de(),
       donate: {
