2815b538cf195c990fad3da3edb0fdd7f4d01b73
no8ody <no8ody@tanglebay.com>
2020-03-26 11:00:01 +0100

    fix footer lang

--- src/plugins/translations/footer.js
+++ src/plugins/translations/footer.js
@@ -2,25 +2,7 @@
 const en = function() {
     return {
         ei_mission: 'IOTA simple and easy.',
-        ei_description: 'Our mission is to provide content around IOTA and to explain IOTA in simple language.',
-        title: 'einfachIOTA',
-        blog: 'Blog',
-        magazine: 'Magazin',
-        community: 'Community',
-        partners: 'Partner',
-        iota_title: 'IOTA',
-        iota_web: 'Offizielle Webseite',
-        iota_news: 'News',
-        iota_more: 'Mehr',
-        imprint: 'Impressum',
-        privacy_policy: 'Datenschutz'
-      }
-}
-
-const de = function() {
-    return {
-        ei_mission: 'Unsere Aufgabe ist es, deutschsprachigen Inhalt rund um IOTA bereitzustellen und IOTA in einfacher Sprache zu erklären.',
-        ei_description: 'IOTA einfach erklärt.',
+        ei_description: 'Our mission is to provide content around IOTA and to explain IOTA in simple way.',
         title: 'einfachIOTA',
         blog: 'Blog',
         magazine: 'Magazine',
@@ -31,7 +13,25 @@ const de = function() {
         iota_news: 'News',
         iota_more: 'More',
         imprint: 'Imprint',
-        privacy_policy: ' Privacy Policy'
+        privacy_policy: 'Privacy Policy'
+      }
+}
+
+const de = function() {
+    return {
+        ei_mission: 'Unsere Aufgabe ist es, deutschsprachigen Inhalt rund um IOTA bereitzustellen und IOTA auf einfachem Weg zu erklären.',
+        ei_description: 'IOTA einfach erklärt.',
+        title: 'einfachIOTA',
+        blog: 'Blog',
+        magazine: 'Magazin',
+        community: 'Community',
+        partners: 'Partner',
+        iota_title: 'IOTA',
+        iota_web: 'Offizielle Webseite',
+        iota_news: 'Neuigkeiten',
+        iota_more: 'Mehr',
+        imprint: 'Impressum',
+        privacy_policy: 'Datenschutzerklärung'
       }
 }
 module.exports = {
